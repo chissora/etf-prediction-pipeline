09c714dc0132ca06a48e7d21e989c655e2f72a16
Dominik Olszowy <midonik10@gmail.com>
2026-06-15 19:57:32 +0200

    add pipeline entry point

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from src.download import main as download_data
+from src.features import main as generate_features
 
 
 def main():
     download_data()
+    generate_features()
 
 
 if __name__ == "__main__":
